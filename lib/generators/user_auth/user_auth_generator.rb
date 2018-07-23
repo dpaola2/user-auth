be0fb9de79aaac3e2dee4a3fe5f6a4744cc69839
Dave Paola <dpaola2@gmail.com>
2018-07-23 14:18:04 -0700

    generator code

---
+++ lib/generators/user_auth/user_auth_generator.rb
@@ -0,0 +1,45 @@
+class UserAuthGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def perform
+    gem "bcrypt"
+
+    routes.each do |r|
+      route r
+    end
+
+    generate "model", "User email:string password_digest:string admin:boolean remember_digest:string"
+
+    file_list.each do |f|
+      copy_file f, f
+    end
+  end
+
+  private
+
+  def file_list
+    [
+      "app/models/user.rb",
+      "app/controllers/sessions_controller.rb",
+      "app/controllers/users_controller.rb",
+      "app/helpers/sessions_helper.rb",
+      "app/views/sessions/_login.html.erb",
+      "app/views/sessions/new.html.erb",
+      "app/views/users/_form.html.erb",
+      "app/views/users/edit.html.erb",
+      "app/views/users/show.html.erb",
+      "app/views/users/new.html.erb"
+    ]
+  end
+
+  def routes
+    [
+      "get '/signup',     to: 'users#new'",
+      "get '/login',      to: 'sessions#new'",
+      "post '/login',     to: 'sessions#create'",
+      "delete '/logout',  to: 'sessions#destroy'",
+      "resources :users, except: [:index]"
+    ]
+  end
+end
+
