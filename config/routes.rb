6523de1ea79d681236e912ede25e3b4a0c4235e9
Dave Paola <dpaola2@gmail.com>
2018-07-23 14:21:22 -0700

    remove old files

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  get '/signup',     to: 'users#new'
-  get '/login',      to: 'sessions#new'
-  post '/login',     to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
-
-  resources :users, except: [:index]
-end
